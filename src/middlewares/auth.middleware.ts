06d18e7518dabf6660758580282179c14e7b39a5
Ankit Kumar Singh <ankitsinghsjs@gmail.com>
2021-05-29 08:09:10 +0900

    Updated to use cookie

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -5,26 +5,33 @@ import CustomException from '../exceptions/custom.exception';
 import { DataStoredInToken } from '../interfaces/tokenData.interface';
 import { logger } from '../utils/logger';
 
-async function authMiddleWare(req: express.Request, res: express.Response, next: express.NextFunction):Promise<void> {
-    const header = req.headers;
-    if(header && header.authorization){
-        const secret = process.env['JWT_SECRET']||'';
+async function authMiddleWare(req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> {
+    res.type('json');
+    const cookie = req.cookies;
+    if (cookie && cookie.Authorization) {
+        const secret = process.env['JWT_SECRET'] || '';
         try {
-            const verificationRes = JWT.verify(header.authorization, secret) as DataStoredInToken;
+            const verificationRes = JWT.verify(cookie.Authorization, secret) as DataStoredInToken;
             const username = verificationRes._id;
-            const user = 'ankit';
-            if(user != username){
-                next(new CustomException('WrongAuthenticationTokenException', 'Incorrect Auth. Token'));
-            }else{
-                logger.info('[Token Verification] [PASSED] for: %s',username);
+            const user = 'ankit@somedomain.com';
+            if (user != username) {
+                const exception = new CustomException('WrongAuthenticationTokenException', 'Incorrect Auth. Token');
+                res.status(400).send(exception.toJSON());
+                // next(exception);
+            } else {
+                logger.info('[Token Verification] [PASSED] for: %s', username);
                 next();
             }
         } catch (error) {
-            next(new CustomException('WrongAuthenticationTokenException', error.message));
+            const exception = new CustomException('WrongAuthenticationTokenException', error.message);
+            res.status(400).send(exception.toJSON());
+            // next(exception);
         }
-    }else{
-        next(new CustomException('MissingAuthorizationTokenException','Could not find the token in header'));
+    } else {
+        const exception = new CustomException('MissingAuthorizationTokenException', 'Could not find the token in cookie');
+        res.status(400).send(exception.toJSON());
+        // next(exception);
     }
-    return; 
+    return;
 }
 export default authMiddleWare;
