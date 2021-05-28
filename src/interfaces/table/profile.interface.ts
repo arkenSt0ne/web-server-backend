76ca1d79ecfd1d9ce44caf0cd3ab25a9a6b5f784
Ankit Kumar Singh <ankitsinghsjs@gmail.com>
2021-05-29 05:58:27 +0900

    Updated the Attributes

--- src/interfaces/table/profile.interface.ts
+++ src/interfaces/table/profile.interface.ts
@@ -1,12 +1,12 @@
 import { Optional } from 'sequelize';
 
 export interface UserProfileAttributes {
-    id: string;
+    id?: string;
     profileId:string;
     name: string;
     gender: string;
-    location: string;
-    website: string;
-    picture: string;
+    location?: string;
+    website?: string;
+    picture?: string;
 }
 export type UserProfileCreationAttributes = Optional<UserProfileAttributes, 'profileId'>;
--- src/interfaces/table/user.interface.ts
+++ src/interfaces/table/user.interface.ts
@@ -1,12 +1,12 @@
 import {Optional} from 'sequelize';
 export interface UserAttributes {
-    id: string;
+    id?: string;
     email: string;
     password: string;
-    passwordResetToken: string;
-    passwordResetExpires: Date;
-    facebook: string;
-    tokenId: string; // fk
-    profileId: string; // fk
+    salt:string;
+    passwordResetToken?: string;
+    passwordResetExpires?: Date;
+    facebook?: string;
+    tokenId?: string; // fk
 }
 export type UserCreationAttributes = Optional<UserAttributes, 'email'>;
