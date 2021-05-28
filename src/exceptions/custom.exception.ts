7c6e5601b835c7d2db8b1efff2c25350163aa699
Ankit Kumar Singh <ankitsinghsjs@gmail.com>
2021-05-29 08:07:02 +0900

    Added toJSON method

--- src/exceptions/custom.exception.ts
+++ src/exceptions/custom.exception.ts
@@ -1,6 +1,12 @@
 export default class CustomException extends Error{
+    public message!:string;
+    public name!:string;
     constructor(name:string, message:string){
         super(message);
         this.name = name;
+        this.message = message;
+    }
+    public toJSON(): string {        
+        return `{"error":"${this.name}","message":"${this.message}"}`;
     }
 }
